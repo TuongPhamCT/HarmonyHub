c92eb8b2ef48df6c7f3e2c4e92821aca052add30
HuynhDangQuang <hdquang437@gmail.com>
2024-12-19 17:44:34 +0700

    Implement Playbar

---
+++ harmonyhub/src/components/AlbumsPage/services/allAlbumServices.js
@@ -0,0 +1,9 @@
+export const loadAllNewReleaseAlbums = () => {
+  // trả về tất cả SongModel sắp xếp theo ngày phát hành
+  return [];
+}
+
+export const loadAllTopAlbums = () => {
+  // trả về tất cả SongModel sắp xếp theo playCount
+  return [];
+}
