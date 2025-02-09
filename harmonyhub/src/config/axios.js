207d513b64775e0c89054e9ccd20d695b50538a2
phamdatzx <megumikatoubuhbuh@gmail.com>
2025-02-09 14:37:33 +0700

    config cors

--- harmonyhub/src/config/axios.js
+++ harmonyhub/src/config/axios.js
@@ -1,11 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5000',
+  baseURL: "http://localhost:5000",
 });
 
-const accessToken = localStorage.getItem('accessToken');
+const accessToken = localStorage.getItem("accessToken");
 const AUTH_TOKEN = `Bearer ${accessToken}`; // Replace with your actual auth token
-instance.defaults.headers.common['Authorization'] = AUTH_TOKEN;
+instance.defaults.headers.common["Authorization"] = AUTH_TOKEN;
 
 export default instance;
--- my-app-backend/index.js
+++ my-app-backend/index.js
@@ -11,7 +11,20 @@ const artistRoute = require("./routes/artist.route");
 const genreRoute = require("./routes/genre.route");
 const playlistRoute = require("./routes/playlist.route");
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  cors({
+    origin: "*",
+    methods: "*",
+    allowedHeaders: "*",
+    exposedHeaders: "*",
+    credentials: true,
+    preflightContinue: false, // Add this to ensure proper OPTIONS handling
+    optionsSuccessStatus: 204, // Some legacy browsers (IE11, various SmartTVs) choke on 204
+  })
+);
+
+app.options("*", cors()); // enable pre-flight request for ALL routes
+app.use("/public", express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(cors());
 app.use("", authRoute);
